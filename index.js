a86db4d2401bbafc7059e4c317c69807ac3549fa
Daniel Rotchford <jimrockford45@gmail.com>
2017-09-16 14:33:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 happyHolidaysTo(`you`)
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${firstname}`
+  return 'Happy ${holiday}, ${firstname}'
 }
 happyHolidayTo (`Independence Day,`, `you!`)
