286f7e1f73e506636e2e297021c811f9d44d5781
Daniel Rotchford <jimrockford45@gmail.com>
2017-09-16 14:42:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function happyHolidayTo(holiday, firstname) {
   return `Happy ${holiday}, ${firstname}!`
 }
 happyHolidayTo(`Independence Day,`, `you`)
+
+function happyCountdown(holiday, days) {
+  return `It's ${days} until {holiday}!`
+}
